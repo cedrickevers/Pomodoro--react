82af10a3d19b91c950fa6c11998880c5083f0df7
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-09-02 23:34:07 +0200

    Setting timer with probs and type variable

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ import ReactDom from 'react-dom';
 
 const Header = () => <h1>Pomodoro</h1>
 
+const SetTimer = ({ type }) =>(
+    <div>
+        <div id={`${type}-label`}> {`${type} length`} </div>
+    </div>
+)
+
 class App extends React.Component {
     render  () {
         return (
             <div>
                 <Header/>
+                <div>
+                    <SetTimer type = "break"/>
+                    <SetTimer type="session"/>
+                </div>
             </div>
         )
     }
