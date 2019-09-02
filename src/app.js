cf0c979976d9d2f2d435303e2c7232fb82d0582e
ck <keverscedric@gmail.com>
2019-09-02 12:14:50 +0200

    showing wesh world with reactDom app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return (<div>
+        <h1> Wesh World</h1>
+    </div>)
+}
+ReactDom.render(<App />, document.getElementById("test"))
