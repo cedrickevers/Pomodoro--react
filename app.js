f11ab546db8f5112e443aaa99fa875fa586e930d
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-09-03 10:37:09 +0200

    Add values and className

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React, { useState, useEffect } from 'react';
 
-const Header = () => <h1>Pomodoro</h1>
+const Timer = () => {
+    const [seconds, setSeconds] = useState(0);
+    const [isActive, setIsActive] = useState(false);
 
-const SetTimer = ({ type }) =>(
-    <div>
-        <div id={`${type}-label`}> {`${type} length`} </div>`
-    <button id={`${type}-decrement`}>test button</button> 
-    <button id={`${type}-increment`}>test button</button>
-
-    </div>
-    
-)
-
-class App extends React.Component {
-    render  () {
-        return (
-            <div>
-                <Header/>
-                <div>
-                    <SetTimer type = "break"/>
-                    <SetTimer type="session"/>
-                </div>
-            </div>
-        )
+    function toggle() {
+        setIsActive(!isActive);
     }
- 
 
-}
-ReactDom.render(<App />, document.getElementById("test"))
+    function reset() {
+        setSeconds(0);
+        setIsActive(false);
+    }
+
+    useEffect(() => {
+        let interval = null;
+        if (isActive) {
+            interval = setInterval(() => {
+                setSeconds(seconds => seconds + 1);
+            }, 1000);
+        } else if (!isActive && seconds !== 0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isActive, seconds]);
+
+    return (
+        <div className="app">
+            <div className="time">
+                {seconds}s
+      </div>
+            <div className="row">
+                <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+                    {isActive ? 'Pause' : 'Start'}
+                </button>
+                <button className="button" onClick={reset}>
+                    Reset
+        </button>
+            </div>
+        </div>
+    );
+};
+
+export default Timer;
