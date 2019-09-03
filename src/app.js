261dce79c67ebb8c6ea5be6dec8955acb540ed6a
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-09-03 13:54:17 +0200

    add const controllers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React, { useState, useEffect } from 'react';
 
-const App = () => {
-    return (<div>
-        <h1> Wesh World</h1>
-    </div>)
-}
-ReactDom.render(<App />, document.getElementById("test"))
+const Timer = () => {
+    const [seconds, setSeconds] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+
+    function toggle() {
+        setIsActive(!isActive);
+    }
+
+    function reset() {
+        setSeconds(0);
+        setIsActive(false);
+    }
+
+    useEffect(() => {
+        let interval = null;
+        if (isActive) {
+            interval = setInterval(() => {
+                setSeconds(seconds => seconds + 1);
+            }, 1000);
+        } else if (!isActive && seconds !== 0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isActive, seconds]);
+
+    return (
+        <div className="app">
+            <div className="time">
+                {seconds}s
+      </div>
+            <div className="row">
+                <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+                    {isActive ? 'Pause' : 'Start'}
+                </button>
+                <button className="button" onClick={reset}>
+                    Reset
+        </button>
+            </div>
+        </div>
+    );
+};
+
+export default Timer;
